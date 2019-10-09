f757d1c0b83fb7da754b22c62f7257459a1c928c
mohammed <ibrahimdamy@gmail.com>
2019-10-09 16:39:58 +0100

    responsive

--- src/middlewares/middlewares.js
+++
@@ -1,5 +0,0 @@
-import renderLandingPage from './renderLandingPage';
-import trimUrl from './trim-url';
-import deleteUrl from './delete-url';
-
-export default { renderLandingPage, trimUrl, deleteUrl };
--- src/models/UrlShorten.js
+++ src/models/UrlShorten.js
@@ -1,13 +1,11 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
-
 const urlShortenSchema = new Schema({
-	long_url: { type: String, required: true },
-	clipped_url: { type: String, required: true },
-	urlCode: { type: String, required: true },
-	created_by: { type: String, required: true },
-	createdAt: { type: Date, default: Date.now },
-	click_count: { type: Number, required: true }
+    long_url: String,
+    clipped_url: String,
+    urlCode: String,
+    created_by: String,
+    createdAt: { type: Date, default: Date.now },
+    click_count: Number,
 });
-
-export default mongoose.model("UrlShorten", urlShortenSchema);
+mongoose.model("UrlShorten", urlShortenSchema);
